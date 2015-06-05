ca4d3ee69181749db2dea514fa1ef88421a3b426
Wes Breisch <wabreisch@quinnipiac.edu>
2015-06-05 14:02:11 -0400

    - Updates code up through Chapter 5.5 of Discovering Meteor

--- server/fixtures.js
+++ server/fixtures.js
@@ -13,9 +13,4 @@ if (Posts.find().count() === 0) {
     	title: 'The Meteor Book',
     	url: 'http://themeteorbook.com'
 	});
-
-	Posts.insert({
-    	title: 'Google',
-    	url: 'http://google.com'
-	});
 }
