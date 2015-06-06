8b0fd3f7dace948696602cb6d8f77ff74a821300
Wes Breisch <wabreisch@quinnipiac.edu>
2015-06-06 10:41:07 -0400

    - Adds functionality to prevent post URL collisions

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,7 +6,15 @@ Template.postSubmit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		};
-		post._id = Posts.insert(post);
-		Router.go('postPage', post);
+
+		Meteor.call('postInsert', post, function(error, result) {
+			if ( error )
+				return alert(error.reason);
+
+			if ( result.postExists )
+				alert('This link has already been posted.');
+
+			Router.go('postPage', {_id: result._id});
+		});
 	}
 });
