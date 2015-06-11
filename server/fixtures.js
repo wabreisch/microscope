c89f5d20b1c804c435c398785d6a1d94412e7101
Wes Breisch <wabreisch@quinnipiac.edu>
2015-06-11 12:23:30 -0400

    - Adds basic commenting functionality (can only add comments from the command line right now!)

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,57 @@
+// Fixture data
+
 if (Posts.find().count() === 0) {
-	Posts.insert({
-    	title: 'Introducing Telescope',
-    	url: 'http://sachagreif.com/introducing-telescope/'
-  	});
+  var now = new Date().getTime();
 
-  	Posts.insert({
-    	title: 'Meteor',
-    	url: 'http://meteor.com'
-	});
+  // create two new users
+  var tomId = Meteor.users.insert({
+    profile: { name: 'Tom Coleman'}
+  });
 
-  	Posts.insert({
-    	title: 'The Meteor Book',
-    	url: 'http://themeteorbook.com'
-	});
+  var jeffId = Meteor.users.insert({
+    profile: { name: 'Jeff Doge' }
+  });
+
+  var tom = Meteor.users.findOne(tomId);
+  var jeff = Meteor.users.findOne(jeffId);
+
+  var telescopeId = Posts.insert({
+    title: 'Introducing Telescope',
+    userId: jeff._id,
+    author: jeff.profile.name,
+    url: 'http://sachagreif.com/introducing-telescope/',
+    submitted: new Date(now - 7 * 3600 * 1000)
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: tom._id,
+    author: tom.profile.name,
+    submitted: new Date(now - 5 * 3600 * 1000),
+    body: 'Interesting project!'
+  });
+
+  Comments.insert({
+    postId: telescopeId,
+    userId: jeff._id,
+    author: jeff.profile.name,
+    submitted: new Date(now - 3 * 3600 * 1000),
+    body: 'Thanks bro!'
+  });
+
+  // Posts.insert({
+  //   title: 'Meteor',
+  //   userId: tom._id,
+  //   author: tom.profile.name,
+  //   url: 'http://meteor.com',
+  //   submitted: new Date(now - 10 * 3600 * 1000)
+  // });
+
+  // Posts.insert({
+  //     title: 'The Meteor Book',
+  //     userId: tom._id,
+  //     author: tom.profile.name,
+  //     url: 'http://themeteorbook.com',
+  //     submitted: new Date(now - 12 * 3600 * 1000)
+  //   });
 }
