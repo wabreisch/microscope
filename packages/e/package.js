3ff74472b7e253f5132a2c1764c9147176aa83fe
Wes Breisch <wabreisch@quinnipiac.edu>
2015-06-11 11:06:42 -0400

    - Updates package.js

--- packages/e/package.js
+++ packages/e/package.js
@@ -2,10 +2,8 @@ Package.describe({
   name: 'wabreisch:e',
   summary: "A pattern to display application errors to the user",
   version: '1.0.0',
-  // Brief, one-line summary of the package.
-  summary: '',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/wabreisch/microscope/tree/master/packages/e',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
