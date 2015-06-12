a3c0fa92ee65ec69554fa164a07791170ae24fef
Wes Breisch <wabreisch@quinnipiac.edu>
2015-06-12 13:26:14 -0400

    - Ensures we don't use the plural 'comments' for when there is just one comment on a post

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -6,5 +6,8 @@ Template.postItem.helpers({
 		var a = document.createElement('a');
 		a.href = this.url;
 		return a.hostname;
+	},
+	hasOneComment: function () {
+		return this.commentsCount === 1;
 	}
 });
